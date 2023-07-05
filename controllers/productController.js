35ac9a51fb51c4a1ae90aa42ef33560ae2955db3
ARPAN CHOUDHURY <123379937+DataWorker2001@users.noreply.github.com>
2023-07-05 08:10:59 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -76,7 +76,7 @@ exports.deleteProduct = async (req, res) => {
     if (!product) {
       return res.status(404).json({ error: 'Product not found' });
     }
-    res.json(product);
+    // res.json(product);
     res.json({ message: "Requested Product Deleted Successfully!" });
   } catch (err) {
     res.status(500).json({ error: 'Failed to delete the product' });
