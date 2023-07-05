c153ea29d9779c64e68dd1775588efd221eed5f2
ARPAN CHOUDHURY <123379937+DataWorker2001@users.noreply.github.com>
2023-07-05 07:19:42 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const dbURI = 'mongodb+srv://Arpan2001:Arpan2023@cluster0.hciafyn.mongodb.net/?retryWrites=true&w=majority/ecom';
+// const dbURI = 'mongodb+srv://Arpan2001:Arpan2023@cluster0.hciafyn.mongodb.net/?retryWrites=true&w=majority/ecom';
+const dbURI = 'mongodb://atlas-sql-64a4c72a850c220cc358ab52-uhbfl.a.query.mongodb.net/test?ssl=true&authSource=admin';
+
  // Replace 'your-database-name' with your actual database name
 
 mongoose.connect(dbURI, {
